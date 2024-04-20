b04bf1711d7b91563f375e0e8f2dedefbff95906
Godwin Idemudia <godwinide@gmail.com>
2024-04-20 21:07:43 +0100

    new

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const nodeMailer = require("nodemailer");
+
 
 router.get("/", (req, res) => {
     try {
@@ -51,6 +53,55 @@ router.get("/faq", (req, res) => {
     }
     catch (err) {
         return res.redirect("/");
+    }
+});
+
+
+router.post("/sendemail", async (req, res) => {
+    try {
+        const { subject, body, email, username, phone } = req.body;
+
+        let transporter = nodeMailer.createTransport({
+            host: "smtp.zoho.com",
+            secure: true,
+            port: 465,
+            auth: {
+                user: "info@avadigitaltrade.pro",
+                pass: "TgzCkyHYTkdA",
+            },
+        })
+
+        const mailOptions = {
+            from: "info@avadigitaltrade.pro",
+            to: "avafxtradez@aol.com",
+            subject,
+            html: `<div>
+                <p>
+                    Name: ${username}
+                </p>
+                <p>
+                    Email: ${email}
+                </p>
+                <p>
+                    Phone Number: ${phone}
+                </p>
+                <p>
+                    ${body}
+                </p>
+            </div>`,
+        };
+
+        await transporter.sendMail(mailOptions, function (err, info) {
+            if (err) {
+                console.log(err);
+            }
+        });
+
+        req.flash("success_msg", "Feedback send successfully.")
+
+        res.redirect("/#email")
+    } catch (err) {
+        console.log(err);
     }
 });
 
